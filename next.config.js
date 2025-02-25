e66d02433efda6672e530d02cd071ea7ca8832fd
itsfooxy1992 <eu.feehenrique@gmail.com>
2025-02-25 19:53:24 +0000

    innit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'], // Add any other image domains you're using
+  },
 }
 
 module.exports = nextConfig 
